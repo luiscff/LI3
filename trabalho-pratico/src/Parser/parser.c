c412881615abf62f605bae38eaa3bcb08b88849d
Luís <a100549@alunos.uminho.pt>
2023-11-11 19:06:23 +0000

    inicio do parser

--- trabalho-pratico/src/Parser/parser.c
+++ trabalho-pratico/src/Parser/parser.c
@@ -6,3 +6,50 @@
 #define MAX_PASSPORT 8
 #define MAX_COUNTRY_CODE 2
 #define MAX_DATE_TIME 20
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void parseCSV(const char *filepath) {
+    FILE *file = fopen(filepath, "r");
+
+    if (file == NULL) {
+        printf("Could not open file %s\n", filepath);
+        return;
+    }
+
+    char *line = NULL;
+    size_t len = 0;
+    size_t read;
+
+    while ((read = getline(&line, &len, file)) != -1) {
+        char *token;
+
+        // get the first token
+        token = strtok(line, ";");
+
+        // walk through other tokens
+        while (token != NULL ) { //TODO dá print de um espaço vazio
+            printf(" %s\n", token);
+            token = strtok(NULL, ";");
+        }
+    }
+
+    free(line);
+    fclose(file);
+}
+
+
+//path do input é ignorado para já
+int main(int argc, char const *argv[]) {
+    if (argc != 3) {
+        printf("Usage: %s <filePath> <inputPath>\n", argv[0]);
+        return 1;
+    }
+
+    const char *filepath = argv[1];
+    parseCSV(filepath);
+
+    return 0;
+}
