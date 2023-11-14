fa7277b164f0d17bfb15ea3cc11a0a5d143b426b
fabs <fabsin4k@gmail.com>
2023-11-14 23:11:34 +0100

    Add dos file.h das entidades

--- trabalho-pratico/include/Entities/reservation.h
+++ trabalho-pratico/include/Entities/reservation.h
@@ -1,6 +1,3 @@
-//
-// Created by fabexfofinho on 14/11/2023.
-//
 
 #ifndef LI3_RESERVATION_H
 #define LI3_RESERVATION_H
--- trabalho-pratico/include/Entities/user.h
+++ trabalho-pratico/include/Entities/user.h
@@ -1,6 +1,3 @@
-//
-// Created by fabexfofinho on 14/11/2023.
-//
 
 #ifndef LI3_USER_H
 #define LI3_USER_H
